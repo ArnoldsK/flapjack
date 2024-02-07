d0f345acdaeefcc9b09e2599a5dc5e59b863456d
ArnoldsK <arnolds0klavenieks@gmail.com>
2024-02-07 16:42:24 +0200

    xD

--- server/cron/creditsBankHourlyInterest.ts
+++ server/cron/creditsBankHourlyInterest.ts
@@ -5,7 +5,7 @@ import { CronTask } from "../utils/cron"
 export default {
   description: "Credits bank hourly interest",
 
-  expression: "every 5 seconds",
+  expression: "every hour",
 
   async execute() {
     const { rate, divider } = CREDITS_BANK_HOURLY_RATE
