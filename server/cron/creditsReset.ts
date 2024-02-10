87047c73e2dccaa53e613f698ada571736c6d048
ArnoldsK <arnolds0klavenieks@gmail.com>
2024-02-10 15:00:50 +0200

    credits reset

---
+++ server/cron/creditsReset.ts
@@ -0,0 +1,40 @@
+import { appConfig } from "../config"
+import { Color } from "../constants"
+import CreditsEntity from "../entity/Credits"
+import { CronTask } from "../utils/cron"
+
+export default {
+  description: "Credits reset",
+
+  // At 12:00 AM, on day 1 of the month, every 3 months
+  expression: "0 0 0 1 */3 *",
+
+  isRawExpression: true,
+
+  productionOnly: true,
+
+  async execute(client) {
+    await CreditsEntity.createQueryBuilder().delete().execute()
+
+    const embeds = [
+      {
+        color: Color.red,
+        title: `All credits have been reset`,
+      },
+    ]
+
+    const channelIds = [
+      appConfig.discord.ids.channels.casino,
+      appConfig.discord.ids.channels.logs,
+    ]
+
+    await Promise.all(
+      channelIds.map(async (id) => {
+        const channel = client.channels.cache.get(id)
+        if (channel?.isTextBased()) {
+          await channel.send({ embeds })
+        }
+      }),
+    )
+  },
+} as CronTask
