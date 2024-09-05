41c9ee5ea3475ba8b3d840bcfdea465ff37fbd63
ArnoldsK <arnolds0klavenieks@gmail.com>
2024-09-05 14:47:00 +0300

    Update left embed

--- server/events/logGuildMemberRemove.ts
+++ server/events/logGuildMemberRemove.ts
@@ -6,30 +6,30 @@ import { isTextChannel } from "../utils/channel"
 import { joinAsLines } from "../utils/string"
 import { Color } from "../constants"
 import { d } from "../utils/date"
+import { embedAuthor } from "../utils/member"
 
 export default createEvent(
   Events.GuildMemberRemove,
   { productionOnly: true },
   async (_context, member) => {
-    const ch = member.guild.channels.cache.get(discordIds.channels.logs)
-    if (!isTextChannel(ch)) return
+    if (member.partial) return
 
-    const name =
-      member.displayName === member.user.username
-        ? `<@${member.id}>`
-        : `<@${member.id}> (${member.user.username})`
+    const channel = member.guild.channels.cache.get(discordIds.channels.logs)
+    if (!isTextChannel(channel)) return
+
     const joinedAt = d(member.joinedAt).fromNow()
     const roles = member.roles.cache
       .filter((role) => role.id !== role.guild.id)
       .map((role) => role.name)
 
-    ch.send({
+    channel.send({
       embeds: [
         {
           color: Color.black,
+          author: embedAuthor(member),
+          title: "Left the server",
           description: joinAsLines(
             ...[
-              `${name} has left the server`,
               `Joined: ${joinedAt}`,
               roles.length ? `Roles: ${roles.join(", ")}` : "",
             ].filter(Boolean),
