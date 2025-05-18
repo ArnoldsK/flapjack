52f041b105c7bd845c1f41ff6ce56a9ee0ddf318
ArnoldsK <arnolds0klavenieks@gmail.com>
2025-05-18 16:15:21 +0300

    Use one query for deletion

--- server/cron/tasks/removeOldStats.ts
+++ server/cron/tasks/removeOldStats.ts
@@ -6,16 +6,10 @@ import { StatsEntity } from "../../db/entity/Stats"
 export const removeOldStats: Task = async (_context) => {
   const minDate = dayjs().subtract(6, "months")
 
-  const entities = await StatsEntity.createQueryBuilder()
+  await StatsEntity.createQueryBuilder()
+    .delete()
     .where("`timestamp` < :ts", {
       ts: minDate.unix(),
     })
-    .getRawMany()
-
-  if (entities.length) {
-    await StatsEntity.createQueryBuilder()
-      .delete()
-      .whereInIds(entities.map(({ id }) => id))
-      .execute()
-  }
+    .execute()
 }
