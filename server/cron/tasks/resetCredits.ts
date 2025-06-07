091229ebb49a37b84aef834aaf8cfdd182a1e902
ArnoldsK <arnolds0klavenieks@gmail.com>
2025-06-07 19:17:10 +0300

    Display bot credits during credits reset

--- server/cron/tasks/resetCredits.ts
+++ server/cron/tasks/resetCredits.ts
@@ -1,16 +1,24 @@
+import { APIEmbed } from "discord.js"
+
 import { DISCORD_IDS } from "~/constants"
 import { Color } from "~/constants"
 import { CreditsModel } from "~/server/db/model/Credits"
+import { formatCredits } from "~/server/utils/credits"
 import { Task } from "~/types/tasks"
 
 export const resetCredits: Task = async (context) => {
   const model = new CreditsModel(context)
+
+  const botUser = context.guild().client.user
+  const botWallet = await model.getWallet(botUser.id)
+
   await model.removeAll()
 
-  const embeds = [
+  const embeds: APIEmbed[] = [
     {
       color: Color.red,
       title: `All credits have been reset`,
+      description: `${botUser.displayName} had ${formatCredits(botWallet.credits)}`,
     },
   ]
 
